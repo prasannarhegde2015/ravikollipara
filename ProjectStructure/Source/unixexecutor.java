c68fff85eb1a2d19623526bfeb398e8e337b2770
prasannarhegde2015 <prasannarhegde@gmail.com>
2017-09-05 00:09:14 +0530

    Create unixexecutor.java

---
+++ ProjectStructure/Source/unixexecutor.java
@@ -0,0 +1,13 @@
+package com.test.firefox;
+
+public class unixexecutor {
+
+	public void tc3_unixexe() {
+		// to be executed layter
+		// String command="C:\\Users\\Prabu\\Documents\\ds.exe \"username1\"
+		// \"password1\"";
+		// Runtime.getRuntime().exec(command);
+
+	}
+
+}
